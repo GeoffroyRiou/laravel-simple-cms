95a5852e7f0b1922399dae5e10cc4ee2eed20e8f
Geoffroy Riou <geoffroy.riou@imageinfrance.com>
2025-03-19 10:50:44 +0100

    first commit

---
+++ database/migrations/2025_03_17_161740_create_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->boolean('published')->default(0);
+            $table->json('page_blocks')->nullable();
+            $table->unsignedBigInteger('parent_id')->nullable();
+
+            $table->foreign('parent_id')->references('id')->on('categories')->onDelete('set null');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
