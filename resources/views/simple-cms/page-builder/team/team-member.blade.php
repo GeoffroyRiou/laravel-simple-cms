698466378c153e23995265f6504bb03202de2333
GeoffroyRiou <geoffroy.riou.pro@gmail.com>
2025-03-20 14:20:44 +0100

    Follow up

--- resources/views/simple-cms/page-builder/team/team-member.blade.php
+++ resources/views/simple-cms/page-builder/team/team-member.blade.php
@@ -1,6 +1,6 @@
 @props(['data' => []])
 
-<div class="group bg-white border border-indigo-200 rounded-lg p-5 text-center">
+<div {{ $attributes->merge(['class' => 'group bg-white border border-indigo-200 rounded-lg p-5 text-center']) }}>
 
     @if (!empty($data['image']))
         <x-simple-cms::image :path="$data['image']" width="150" height="150" crop="true" class="rounded-full mx-auto" />
--- resources/views/simple-cms/page-builder/team/team.blade.php
+++ resources/views/simple-cms/page-builder/team/team.blade.php
@@ -4,9 +4,9 @@
 ])
 
 <section class="py-5 xl:py-16  {{ $bgColor }}">
-    <div class="px-5 mx-auto flex flex-col gap-5 md:grid md:grid-cols-2 lg:grid-cols-3 xl:max-w-10/12">
+    <div class="px-5 mx-auto flex flex-col gap-5 md:flex-row md:flex-wrap md:justify-center xl:max-w-10/12">
         @foreach ($members as $member)
-            <x-simple-cms::page-builder.team.team-member :data="$member"/>
+            <x-simple-cms::page-builder.team.team-member :data="$member" class="md:flex-[1_1_40%] md:max-w-[40%] lg:flex-[1_1_30%] lg:max-w-[30%] xl:flex-[1_1_20%] xl:max-w-[20%]"/>
         @endforeach
     </div>
 </section>
