da957f730cd234f3a2f5392a3a839168ea26c308
GeoffroyRiou <geoffroy.riou.pro@gmail.com>
2025-03-24 15:46:55 +0100

    follow up

--- resources/views/simple-cms/page-builder/faq.blade.php
+++ resources/views/simple-cms/page-builder/faq.blade.php
@@ -14,7 +14,7 @@
         <div class="divide-y divide-indigo-100 rounded-xl border border-indigo-100 bg-white">
             @foreach ($questions as $question)
                 <details class="group p-6 [&_summary::-webkit-details-marker]:hidden">
-                    <summary class="flex cursor-pointer items-center justify-between gap-1.5 text-indigo-950">
+                    <summary class="flex cursor-pointer items-center justify-between gap-1.5">
                         <h2 class="text-lg font-medium">{{ $question['title'] }}</h2>
 
                         <span class="relative size-5 shrink-0">
@@ -34,7 +34,7 @@
                         </span>
                     </summary>
 
-                    <div class="simple-cms-content mt-4 leading-relaxed text-indigo-800">
+                    <div class="simple-cms-content mt-4 leading-relaxed">
                         {!! $question['text'] ?? '' !!}
                     </div>
                 </details>
