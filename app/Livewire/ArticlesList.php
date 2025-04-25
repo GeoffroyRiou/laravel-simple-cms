b8604702dc22feb3eefdcc1ed667007a7a9e26d9
GeoffroyRiou <geoffroy.riou.pro@gmail.com>
2025-04-25 09:00:53 +0200

    Adding articles list block

---
+++ app/Livewire/ArticlesList.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Livewire;
+
+use App\Models\Article;
+use App\Models\ArticleCategory;
+use Livewire\Component;
+use Livewire\WithoutUrlPagination;
+use Livewire\WithPagination;
+
+class ArticlesList extends Component
+{
+    use WithPagination, WithoutUrlPagination;
+
+    public ?string $categoryId = '0';
+
+    public int $perPage = 0;
+
+    public function mount(?int $perPage = 6): void{
+        $this->perPage = $perPage;
+    }
+    
+    public function render()
+    {
+        $articles = Article::when($this->categoryId, function ($query) {
+            $query->where('category_id', $this->categoryId);
+        })->orderBy('created_at', 'desc')->simplePaginate($this->perPage);
+
+        return view('livewire.articles-list', [
+            'articles' => $articles,
+            'categories' => ArticleCategory::all()
+        ]);
+    }
+}
