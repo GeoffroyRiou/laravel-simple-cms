5730ca5f9cc02ce92c20fe40841b921ee76f2a1f
GeoffroyRiou <geoffroy.riou.pro@gmail.com>
2025-05-14 09:17:42 +0200

    calage settings

--- resources/views/components/setting.blade.php
+++ resources/views/components/setting.blade.php
@@ -1,28 +1,25 @@
-<?php
-
-declare(strict_types=1);
-
-?>
 @props(['setting'])
 
-<div {{ $attributes->merge(['class' => 'flex items-center gap-2 group']) }}>
-    @if (!empty($setting['icon']))
-        <x-dynamic-component :component="$setting['icon']" class="h-4 w-4" />
-    @endif
-    @switch($setting['type'])
-        @case('Email')
-        @case('Phone')
-            <a href="{{ $setting['type'] == 'Phone' ? 'tel' : 'mailto' }}:{{ $setting['value'] }}"
-                class="text-sm font-medium hover:text-primary">{{ $setting['value'] }}</a>
-        @break
+@if (!empty($setting))
+    <div {{ $attributes->merge(['class' => 'flex items-center gap-2 group']) }}>
+        @if (!empty($setting->icon))
+            <x-dynamic-component :component="$setting->icon" class="size-5" />
+        @endif
+        <span class="border-r w-px bg-primary dark:bg-light h-3 opacity-50"></span>
+        @switch($setting->type)
+            @case('Email')
+            @case('Phone')
+                <a href="{{ $setting->type == 'Phone' ? 'tel' : 'mailto' }}:{{ $setting->value }}"
+                    class="text-sm font-title font-medium transition-colors hover:underline">{{ $setting->value }}</a>
+            @break
 
-        @case('Text area')
-            <p class="text-sm font-medium">{!! nl2br($setting['value']) !!}</p>
-        @break
+            @case('Text area')
+                <p class="text-sm font-title font-medium">{!! nl2br($setting->value) !!}</p>
+            @break
 
-        @default
-            <p class="text-sm font-medium">{{ $setting['value'] }}</p>
-        @break
-    @endswitch
-</div>
-<?php 
+            @default
+                <p class="text-sm font-title font-medium">{{ $setting->value }}</p>
+            @break
+        @endswitch
+    </div>
+@endif
